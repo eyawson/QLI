06c0ac53c426dc3e00587f2896a3d8c1ec8c23bc
eyawson <ebow.yawson@gmail.com>
2019-12-15 10:53:54 -0500

    feat: added slideshow
    Jquery slideshow added to project. All images added as well

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,20 @@
 console.log('\'Allo \'Allo!');
 
-$('.backgroundTransition').backgroundTransition({
-    backgrounds: [
-        // at least 2 images
-        { src:'1.jpg' },
-        { src:'2.jpg' },
-        { src:'3.jpg' }
-    ],
-    transitionDelay: 5,
-    animationSpeed: 800
-});
+$(function() {
+    $('.backgroundTransition').backgroundTransition({
+        backgrounds:[
+            { src: 'images/1.jpg' },
+            { src: 'images/2.jpg' },
+            { src: 'images/3.jpg' },
+            { src: 'images/4.jpg' },
+            { src: 'images/5.jpg' },
+            { src: 'images/6.jpg' },
+            { src: 'images/7.jpg' },
+            { src: 'images/8.jpg' },
+            { src: 'images/9.jpg' },
+            { src: 'images/10.jpg' },
+        ],
+        transitionDelay: 5,
+        animationSpeed: 500
+    });
+  });
