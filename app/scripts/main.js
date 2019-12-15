1351d83205162e4ba1655ce2716337d9e0b2c0d2
eyawson <ebow.yawson@gmail.com>
2019-12-14 19:09:56 -0500

    feat: added slideshow code
    added code to make background slideshow possible

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
 console.log('\'Allo \'Allo!');
 
+$('.backgroundTransition').backgroundTransition({
+    backgrounds: [
+        // at least 2 images
+        { src:'1.jpg' },
+        { src:'2.jpg' },
+        { src:'3.jpg' }
+    ],
+    transitionDelay: 5,
+    animationSpeed: 800
+});
