8f667aa5a86439f0420bb919409983017df41054
eyawson <ebow.yawson@gmail.com>
2019-12-15 21:56:47 -0500

    fix: added initial code for services
    the code added will allow to complete the services part faster

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
 $(function() {
     $('.backgroundTransition').backgroundTransition({
         backgrounds:[
-            { src: 'images/1.jpg' },
-            { src: 'images/2.jpg' },
-            { src: 'images/3.jpg' },
-            { src: 'images/4.jpg' },
-            { src: 'images/5.jpg' },
-            { src: 'images/6.jpg' },
-            { src: 'images/7.jpg' },
-            { src: 'images/8.jpg' },
-            { src: 'images/9.jpg' },
-            { src: 'images/10.jpg' },
+            { src: 'images/1.jpg', alt: 'skyzone community activity' },
+            { src: 'images/2.jpg', alt: 'halloween group picture' },
+            { src: 'images/3.jpg', alt: 'visit with state legislators' },
+            { src: 'images/4.jpg', alt: 'CVS externship' },
+            { src: 'images/5.jpg', alt: 'BARCS volunteering' },
+            { src: 'images/6.jpg', alt: 'Auntie Anne pretzel class' },
+            { src: 'images/7.jpg', alt: 'christmas decorations prep' },
+            { src: 'images/8.jpg', alt: 'fire department vist' },
+            { src: 'images/9.jpg', alt: 'group activity' },
+            { src: 'images/10.jpg', alt: 'Baltimore museum of Art visit' },
         ],
         transitionDelay: 5,
         animationSpeed: 500
